35b27f97584ae47d4dc830f930d4373e7e2ebb05
JungleMystic <110449242+JungleMystic@users.noreply.github.com>
2022-09-28 12:18:55 +0530

    Delete Project Grocery Basket directory

--- Project Grocery Basket/GroceryBasket/app/src/main/java/com/lrm/grocerybasket/GroceryBasketApplication.kt
+++
@@ -1,8 +0,0 @@
-package com.lrm.grocerybasket
-
-import android.app.Application
-import com.lrm.grocerybasket.data.ItemRoomDatabase
-
-class GroceryBasketApplication: Application() {
-    val database: ItemRoomDatabase by lazy {ItemRoomDatabase.getDatabase(this)}
-}
