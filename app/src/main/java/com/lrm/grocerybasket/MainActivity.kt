95e534491bff81903adab51933dcaba4165852c4
JungleMystic <locharla.rammohan@gmail.com>
2022-09-28 16:49:17 +0530

    Update 3.0

--- app/src/main/java/com/lrm/grocerybasket/MainActivity.kt
+++ app/src/main/java/com/lrm/grocerybasket/MainActivity.kt
@@ -2,6 +2,7 @@ package com.lrm.grocerybasket
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 
--- app/src/main/java/com/lrm/grocerybasket/SettingsFragment.kt
+++ app/src/main/java/com/lrm/grocerybasket/SettingsFragment.kt
@@ -11,6 +11,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.lrm.grocerybasket.databinding.FragmentAddItemBinding
@@ -41,6 +42,14 @@ class SettingsFragment : Fragment() {
 
         binding.apply {
             backButton.setOnClickListener { findNavController().navigateUp() }
+
+            darkThemeSwitch.setOnClickListener {
+                if (darkThemeSwitch.isChecked == true) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                } else {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                }
+            }
 
             expandAction.setOnClickListener {
                 if(InfoTabLL.visibility == View.GONE) {
